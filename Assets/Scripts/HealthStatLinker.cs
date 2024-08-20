517cd92cd3f1368bfbf2af6bb52b9ceaba031a0f
Choi Yiu Hei <blackrc3636@gmail.com>
2024-08-20 21:49:08 +0800

    Really Fixed player health bar

--- Assets/Scripts/HealthStatLinker.cs
+++ Assets/Scripts/HealthStatLinker.cs
@@ -14,14 +14,18 @@ public class HealthStatLinker : MonoBehaviour
         gameObject.GetStatHolder(maxHealthStatId.id).observableVar.onChanged += (old, value) =>
         {
             float diff = value - old;
-            Debug.Log("Diff: " + diff);
             healthOwner.maxHealth.Value = value;
-            healthOwner.health.Value += diff;
             if(healthOwner.health.Value > healthOwner.maxHealth.Value)
                 healthOwner.health.Value = healthOwner.maxHealth.Value;
 
         };
         gameObject.GetStatHolder(maxHealthStatId.id).onChanged.Invoke(gameObject.GetStatHolder(maxHealthStatId.id).Value);
+        GKUtils.RunAfterSeconds(()=>{
+            healthOwner.health.Value = healthOwner.maxHealth.Value;
+        }, 0.05f);
+    }
+    void Start(){
+        HealthOwner healthOwner = GetComponent<HealthOwner>();
         healthOwner.health.Value = healthOwner.maxHealth.Value;
     }
 }
